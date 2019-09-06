a22abb2c0b37d5043d7341f932c100ed065c82aa
catonlan <catonlan@163.com>
2019-09-07 00:02:13 +0800

    检测是否包含在源字符串

--- t011.js
+++ t011.js
@@ -1,5 +1,5 @@
 var s = 'hello world!'
 
-console.log(s.startsWith('hello'))
-console.log(s.endsWith('!'))
-console.log(s.includes('o'))
+console.log(s.startsWith('hello')) // true 表示参数字符串是否在源字符串的头部
+console.log(s.endsWith('!')) // true 表示参数字符串是否在源字符串的尾部
+console.log(s.includes('o')) // true 表示是否包含在源字符串里
