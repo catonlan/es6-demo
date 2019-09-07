b65da01ff4b2a9ae6670622a832393c1fb162cb2
catonlan <catonlan@163.com>
2019-09-07 11:11:06 +0800

    用表达式作为对象属性名

---
+++ publi/t016.js
@@ -0,0 +1,11 @@
+var lastWord = "last word"
+
+var a = {
+    "first word": "hello"
+    [lastWord]:"world"
+}
+
+console.log(a["first word"])
+
+console.log(a[lastWord])
+console.log(a["last word"])
